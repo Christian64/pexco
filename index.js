39db579fac3687b6d72e41edeee57ddb196815a5
adams <adamswever122132@gmail.com>
2024-10-03 02:46:59 +0200

    desarrollo e implementacion de la funcion deleteProject, cambios en la variable rootDir y en la funcion createLanguage

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-const { mkdirSync } = require("node:fs");
+const { mkdirSync, rmdir } = require("node:fs");
+const { platform } = require("node:process");
+const { join } = require("path");
 const { Command } = require("commander");
 const program = new Command();
-const rootDir = process.env.PWD;
+const rootDir = platform === 'win32' ? process.cwd() : process.cwd();
 
 const main = () => {
   program
@@ -13,15 +15,28 @@ const main = () => {
 
   program
     .command("add")
-    .argument("<string>", "name of the langauge")
+    .argument("<string>", "name of the language")
     .description("Add new program language")
-    .action(createLengauge);
+    .action((str) => createLanguage(str));
+
+  program
+    .command("delete")
+    .argument("<string>", "delete project")
+    .description("delete project from a language")
+    .action((project) => deleteProject(project));
 
   program.parse();
 };
 
-function createLengauge(language) {
-  mkdirSync(`${rootDir}/pexco/${language}`, { recursive: true });
+function createLanguage(language) {
+  mkdirSync(join(rootDir, `pexco/${language}`), { recursive: true });
 }
+
+const deleteProject = (project) => {
+  rmdir(join(rootDir, `pexco/${project}`),
+   (err)=> err
+  ? console.log(`Error borrando el proyecto: ${err.message}`) 
+  : console.log(`${project} ha sido borrado con exito!`) );
+};
 
 main();
