c6e1556076114d33bd94b76aae1c6c7f6aedbd62
adams <adamswever122132@gmail.com>
2024-10-05 02:46:55 +0200

    Implement commands for managing projects in the 'pexco' application: Added a delete command to delete a specified project, expecting a project name as an argument. It checks if the project path exists; if it does, it recursively deletes it using fs.rm, displaying a success message, otherwise, it notifies that the file does not exist. Added a search command to search for projects within a specific language, expecting a language name as an argument. It verifies if the corresponding folder for the language exists; if it does, it lists the projects within that folder, otherwise, it informs that the folder cannot be found. Implemented a list command that lists all files and their languages in the main projects folder, iterating through the projects folder and listing the subdirectories for each project, displaying an organized structure with the project name in uppercase. This set of commands enhances project management within the application, allowing users to efficiently delete, search, and list projects.

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const { mkdirSync, rmdir } = require("node:fs");
+const { mkdirSync, existsSync, readdirSync } = require("node:fs");
+const fs = require('fs').promises;
 const { platform } = require("node:process");
 const { join } = require("path");
 const { Command } = require("commander");
 const program = new Command();
+
 const rootDir = platform === 'win32' ? process.cwd() : process.cwd();
 
 const main = () => {
@@ -17,26 +19,73 @@ const main = () => {
     .command("add")
     .argument("<string>", "name of the language")
     .description("Add new program language")
-    .action((str) => createLanguage(str));
+    .action((str) => createProject(str));
 
   program
     .command("delete")
     .argument("<string>", "delete project")
     .description("delete project from a language")
     .action((project) => deleteProject(project));
+  
+  program
+  .command("search")
+  .argument("<string>", "search project")
+  .description("search project from a language")
+  .action((search) => searchProject(search));
+
+  program
+  .command("list")
+  .description("list of files and their languages")
+  .action(lenguageList);
 
   program.parse();
 };
 
-function createLanguage(language) {
+// Function to create a new folder for a project
+function createProject(language) {
   mkdirSync(join(rootDir, `pexco/${language}`), { recursive: true });
 }
 
-const deleteProject = (project) => {
-  rmdir(join(rootDir, `pexco/${project}`),
-   (err)=> err
-  ? console.log(`Error deleting project: ${err.message}`) 
-  : console.log(`${project} has been deleted`) );
+// Function to delete a specific project folder
+const deleteProject = async (project) => {
+  const projectPath = join(rootDir, `pexco/${project}`);
+  
+  try{
+    if(!projectPath){
+        await fs.rm(projectPath, { recursive: true, force: true });
+        console.log(`${project} has been deleted`);
+    }else{
+      console.log('file not exists')
+    }
+  } catch (err) {
+    console.log(`Error deleting project: ${err.message}`);
+  }
+};
+
+// Function to search for a specific project
+const searchProject = (language) => {
+  
+  const languagePath = join(rootDir, `pexco/${language}`);
+
+  if (existsSync(languagePath)) {
+    let projects = readdirSync(languagePath).map((project, index) => `${index +1}- ${project}`)
+    console.log(projects.join('\n'))
+    
+  } else {
+    console.log(`The folder ${languagePath} does not exist.`);
+  }
+
+}
+
+// Function to list the files in each folder
+const lenguageList = () => {
+  const projectPath = join(rootDir, 'pexco');
+
+  readdirSync(projectPath)
+    .forEach(project => {
+    const folders = readdirSync(join(projectPath, project)); 
+    console.log(`<${project.toUpperCase()}>\n${folders.join('\n')}\n`);
+    });
 };
 
 main();
