03f81c80e0e07f2dff89730518ef6c749dbe55f0
Christian64 <christianabreuh@gmail.com>
2024-10-13 16:14:15 -0400

    Fixing test for Adams changes.

--- init.js
+++ init.js
@@ -1,10 +1,33 @@
 import { Command } from "commander";
+import {
+  createProject,
+  searchProject,
+  deleteProject,
+  lenguageList,
+} from "./index.js";
 const program = new Command();
 
 program
   .command("add")
-  .argument("<string>", "name of the langauge")
+  .argument("<string>", "name of the language")
   .description("Add new program language")
-  .action(createLenguage);
+  .action((str) => createProject(str));
+
+program
+  .command("delete")
+  .argument("<string>", "delete project")
+  .description("delete project from a language")
+  .action((project) => deleteProject(project));
+
+program
+  .command("search")
+  .argument("<string>", "search project")
+  .description("search project from a language")
+  .action((search) => searchProject(search));
+
+program
+  .command("list")
+  .description("list of files and their languages")
+  .action(lenguageList);
 
 program.parse();
